40c8ef652d8994f4983fbb0ad06ba854790f0bdd
Vidhya42 <167015267+Vidhya42@users.noreply.github.com>
2024-05-07 23:31:21 -0700

    Add files via upload

---
+++ web.js
@@ -0,0 +1,32 @@
+function dress(){
+    document.getElementById("dress").src="images/dress1.jpg"
+}
+
+function dress1(){
+    document.getElementById("dress").src="images/dress2.jpg"
+}
+
+function cosmetics(){
+    document.getElementById("cosmetics").src="images/cosmetics1.jpg"
+}
+
+function cosmetics1(){
+    document.getElementById("cosmetics").src="images/cosmetics2.jpg"
+}
+
+function watch(){
+    document.getElementById("watch").src="images/watch1.jpg"
+}
+
+function watch1(){
+    document.getElementById("watch").src="images/watch2.jpg"
+}
+
+function slipper(){
+    document.getElementById("slipper").src="images/slipper1.jpg"
+}
+
+function slipper1(){
+    document.getElementById("slipper").src="images/slipper2.jpg"
+}
+
